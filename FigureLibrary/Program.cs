cb91253888a04fe4ce784f30efcb521e6cb6423e
Ilya Myshkin <hak.dok0@gmail.com>
2024-11-29 15:35:11 +0300

    Add project files.

---
+++ FigureLibrary/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FigureLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            
+        }
+    }
+
+    public abstract class Figure
+    {
+        public abstract double Square { get; }
+    }
+
+    public sealed class Circle : Figure
+    {
+        private readonly double _radius;
+        public override double Square => Math.PI * _radius * _radius;
+
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+                throw new ArgumentException("Radius must be positive");
+
+            _radius = radius;
+        }
+    }
+}
